e02e7c736e326b44d7233cfbae0391d158d3885b
aembr <aembr@duck.com>
2023-01-26 15:05:47 +0000

    First Commit

---
+++ src/main/java/com/aembr/ahhelper/AhHelper.java
@@ -0,0 +1,106 @@
+package com.aembr.ahhelper;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.ContainerChest;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Mod(
+        modid = AhHelper.MOD_ID,
+        name = AhHelper.MOD_NAME,
+        version = AhHelper.VERSION
+)
+@Mod.EventBusSubscriber
+public class AhHelper {
+    public static final String MOD_ID = "ah-helper";
+    public static final String MOD_NAME = "AH-Helper";
+    public static final String VERSION = "1.12.2-0.1";
+
+    public static Pattern usesPattern = Pattern.compile("§.(\\d+) §fremaining uses");
+    public static Pattern pricePattern = Pattern.compile("§5§o§9Price: §e\\$(.*)");
+
+    public AhHelper() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public static void onItemTooltip(ItemTooltipEvent event) {
+        if (!(Minecraft.getMinecraft().currentScreen instanceof GuiContainer)) {
+            return;
+        }
+
+        GuiContainer container = (GuiContainer) Minecraft.getMinecraft().currentScreen;
+
+        if (!(container.inventorySlots instanceof ContainerChest)) {
+            return;
+        }
+
+        String containerName = ((ContainerChest) container.inventorySlots).getLowerChestInventory().getName();
+
+        if (!(containerName.equals("Auction House") || containerName.equals("Your Current Listings"))) {
+            return;
+        }
+
+        List<String> tooltip = event.getToolTip();
+        ItemStack itemStack = event.getItemStack();
+        boolean isSellStick = false;
+        int itemCount = 0;
+        String priceString = null;
+        int price = 0;
+        int priceLineIndex = 0;
+
+        for (int i = 0; i < tooltip.size(); i++) {
+            String line = tooltip.get(i);
+            Matcher matcher = pricePattern.matcher(line);
+            if (matcher.matches()) {
+                price = Integer.parseInt(matcher.group(1).replaceAll(",", ""));
+                priceLineIndex = i;
+                priceString = matcher.group(0);
+                break;
+            }
+        }
+
+        if (price == 0) {
+            return;
+        }
+
+        // Match SellSticks
+        if (itemStack.getItem().getUnlocalizedNameInefficiently(itemStack).equals("item.stick")) {
+            if (!(itemStack.hasTagCompound())) {
+                return;
+            }
+
+            NBTTagList lore = itemStack.getTagCompound().getCompoundTag("display").getTagList("Lore", 8);
+            for (int j = 0; j < lore.tagCount(); j++) {
+                String line = lore.getStringTagAt(j);
+                Matcher matcher = usesPattern.matcher(line);
+                if (matcher.matches()) {
+                    itemCount = Integer.parseInt(matcher.group(1));
+                    if (itemCount == 1) { return; }
+                    isSellStick = true;
+                }
+            }
+        }
+
+        if (!(isSellStick)) {
+            if (itemStack.getCount() > 1) {
+                itemCount = itemStack.getCount();
+            } else {
+                return;
+            }
+        }
+
+        int pricePerItem = price / itemCount;
+        String eachText = (isSellStick) ? "/use)" : " each)";
+        tooltip.set(priceLineIndex, priceString + " §7($" + String.format("%,d", pricePerItem) + eachText);
+    }
+}
